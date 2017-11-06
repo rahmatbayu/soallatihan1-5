875c62117d2dc550bd9391795a4c724ccb4dc286
rahmatbayu <32393970+rahmatbayu@users.noreply.github.com>
2017-11-06 23:45:24 +0800

    memasukan pilihan untuk menghitung luas persegi panjang atau segitiga

--- soal2.cpp
+++ soal2.cpp
@@ -5,4 +5,8 @@ int main(){
 	char pilih,A ,B ;
 	int al, p, l, t, L ;
 	
+	
+	cout <<"A : Persegi Panjang"<<endl;
+	cout <<"B : Segitiga"<<endl;
+	cout << " =>>Luas Yang Akan Dihitung A atau B : "; cin>>pilih;
 }
