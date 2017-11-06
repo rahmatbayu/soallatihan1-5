d82b7967e1ec355ad93f02f301dcbb551678014e
rahmatbayu <32393970+rahmatbayu@users.noreply.github.com>
2017-11-06 23:40:28 +0800

    menghitung selisih nilai x dan y

--- soal1.cpp
+++ soal1.cpp
@@ -7,4 +7,16 @@ int main(){
 	cout<<" Input Bilangan x : "; cin>>x;
 	cout<<" Input Bilangan y : "; cin>>y;
 	
+	if (x>y){
+	selisih=x-y;
+	
+	cout<<"Selisih Nilai X dan Y yaitu "<<selisih;
+}
+
+else if (y>x){
+	selisih=y-x;
+
+	cout<<"Selisih Nilai Y dan X yaitu "<<selisih;
+}
+	return 0;
 }
