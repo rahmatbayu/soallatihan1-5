bf187a70bd5e616b941c08d4d3f6734c1811780f
rahmatbayu <32393970+rahmatbayu@users.noreply.github.com>
2017-11-07 00:02:52 +0800

    memasukan data perhitungan gajih karywan dan meng output kan hasil perhitungan

--- soal3.cpp
+++ soal3.cpp
@@ -7,5 +7,25 @@ int main(){
 	cout<<""<<endl;
 	cout<<"Lama Kerja Karyawan    : "; cin>> jk;
 	cout<<"Gaji Perjam Karyawan   : "; cin>>gp;
+	
+	if (jk>40){
+	system("cls");
+	cout<<"   PROGRAM GAJI BBERSIH KARYAWAN"<<endl;
+	cout<<"     KARYAWAN MENDAPAT LEMBUR"<<endl;
+	cout<<""<<endl;
+	le=jk-40;
+	jam=le*1.5;
+	ul=gp*jam;
+	pa=ul*0.15;
+	gtot=gp-pa;
+	gb=gtot-(jk*40)+((gp-40)*jk*1.5);
+	
+	cout<<"Lama Kerja Karyawan Dalam Seminggu          : "<<jk<<" Jam"<<endl;
+		cout<<"Total Lembur Karyawan                       : "<<le<<" (dalam seminggu)"<<endl;
+		cout<<"Gaji Perjam Yang Karyawan Terima            : Rp  "<<gp<<endl;
+		cout<<"Upah Lembur Yang Diterima Karyawan          : Rp  "<<ul<<endl;
+		cout<<"Pajak Yang Ditanggung Karyawan              : Rp  "<<pa<<endl;
+		cout<<"Total Gaji Bersih Yang Diterima Karyawan    : Rp  "<<gb<<endl;
 
+}
 }
