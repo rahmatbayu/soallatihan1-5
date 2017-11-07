5dfd4d861b6036d880b1dbc885e1aa4159f676c9
rahmatbayu <32393970+rahmatbayu@users.noreply.github.com>
2017-11-07 16:08:23 +0800

    menginputkan proses berat badan dan tinggi badan

--- soal5.cpp
+++ soal5.cpp
@@ -8,4 +8,10 @@ int main(){
     
     cout << "Silahkan masukkan tinggi badan(cm) : "; cin >> tinggibadan;
     cout << "Silahkan masukkan berat badan(kg) : " ; cin >> beratbadan;
-}
+    
+     if (beratbadan<tinggibadan/2.5)
+       cout << "  Anda Mengalami : Underweight(kekurangan berat badan)" << endl;
+        else if (tinggibadan/2.3 < beratbadan)
+       cout << "  Anda Mengalami : Overweight(kelebihan berat badan)" << endl;
+    }
+
