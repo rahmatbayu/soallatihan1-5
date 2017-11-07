e028c829a02dcea1923c20935fb3a753a8839662
rahmatbayu <32393970+rahmatbayu@users.noreply.github.com>
2017-11-07 15:55:52 +0800

    memasukan inputan tinggi badan dan berat badan

--- soal5.cpp
+++ soal5.cpp
@@ -6,4 +6,6 @@ int main(){
 	 float beratbadan;
     int tinggibadan;
     
+    cout << "Silahkan masukkan tinggi badan(cm) : "; cin >> tinggibadan;
+    cout << "Silahkan masukkan berat badan(kg) : " ; cin >> beratbadan;
 }
